7878054a741b6d072fc217f0e37d23f772f5118a
Taiducle <ductaidn99@gmail.com>
2021-08-31 17:10:31 +0700

    change port

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ router.render = (req, res) => {
 };
 
 // Use default router
+const PORT = process.env.PORT || 3000
 server.use("/api", router);
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("JSON Server is running");
 });
