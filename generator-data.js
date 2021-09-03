5751b854e5e55deae7d83e4f3ecfce77af96bce8
Taiducle <ductaidn99@gmail.com>
2021-09-03 10:18:06 +0700

    gennerate data animal

--- generator-data.js
+++ generator-data.js
@@ -2,55 +2,58 @@ const { fake } = require("faker");
 var faker = require("faker");
 var fs = require("fs");
 //set ngon ngu
-faker.locale = "vi";
+faker.locale = "en";
 
-console.log(faker.image.animals);
 
-const randomCategoryList = (n)=>{
-    const categoriesList = []
-    Array.from(new Array(n),()=>{
+const randomCategoryList = (animalList) => {
+    const categoriesList = [];
+    for (let value of animalList) {
         const category = {
-            'id': faker.datatype.uuid(),
-            'name': faker.animal.dog(),
-            'image':faker.image.animals(),
-            'createdAt': Date.now(),
-            'updatedAt': Date.now(),
-        }
-        categoriesList.push(category)
-    })
-    return categoriesList
+            id: faker.datatype.uuid(),
+            name: value,
+            image: faker.image.animals(),
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+        };
+        categoriesList.push(category);
+    }
+    return categoriesList;
 };
-const randomProductList = (categories,n)=>{
-    const productList = []
-     categories.map(({id})=>{
-        for(let i = 0 ; i< n; i++){
+
+const randomProductList = (categories, n) => {
+    const productList = [];
+    categories.map(({ id ,name}) => {
+        for (let i = 0; i < n; i++) {
             const product = {
-                'id': faker.datatype.uuid(),
-                'categoryId': id,
-                'name': faker.animal.dog(),
-                'image':faker.image.animals(),
-                'price':Number.parseFloat(faker.commerce.price()),
-                'createdAt': Date.now(),
-                'updatedAt': Date.now(),
-            }
-            productList.push(product)
+                "id": faker.datatype.uuid(),
+                "categoryId": id,
+                "name": faker.animal[name](),
+                "species" : name,
+                "food": faker.lorem.words(),
+                "weight": Number.parseFloat(faker.commerce.price()),
+                "speed": Number.parseFloat(faker.commerce.price()),
+                "image": faker.image.animals(),
+                "description": faker.lorem.sentence(),
+                "createdAt": Date.now(),
+                "updatedAt": Date.now(),
+            };
+            productList.push(product);
         }
-    })
-    return productList
-}
+    });
+    return productList;
+};
 /* function randomProductList(){
     return []
 } */
-    const categories = randomCategoryList(10);
-     const products = randomProductList(categories,10);
+
+const animalList = ["dog","cat","lion","bear","horse","bird","cow","rabbit"];
+const categories = randomCategoryList(animalList);
+const products = randomProductList(categories, 100);
 
 (() => {
     const db = {
         categories: categories,
         products: products,
-        profile: {
-            name: "po",
-        },
     };
 
     fs.writeFile("db.json", JSON.stringify(db), () => {
